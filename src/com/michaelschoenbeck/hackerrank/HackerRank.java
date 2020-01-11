1c3b4215f0ebba3fd1308d4fd04c68f318f2b44c
mike <mike.schoenbeck@gmail.com>
2020-01-11 18:19:55 -0500

    completed counting valleys

--- src/com/michaelschoenbeck/hackerrank/HackerRank.java
+++ src/com/michaelschoenbeck/hackerrank/HackerRank.java
@@ -1,8 +1,14 @@
 package com.michaelschoenbeck.hackerrank;
 
+import com.michaelschoenbeck.hackerrank.interviewprepkit.warmupchallenges.CountingValleys;
+
+import static com.michaelschoenbeck.hackerrank.interviewprepkit.warmupchallenges.CountingValleys.countingValleys;
+
 public class HackerRank {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println(countingValleys(12, "DDUUDDUDUUUD"));
+
     }
 }
--- src/com/michaelschoenbeck/hackerrank/interviewprepkit/warmupchallenges/CountingValleys.java
+++ src/com/michaelschoenbeck/hackerrank/interviewprepkit/warmupchallenges/CountingValleys.java
@@ -1,4 +1,37 @@
 package com.michaelschoenbeck.hackerrank.interviewprepkit.warmupchallenges;
 
 public class CountingValleys {
+
+
+    public static int countingValleys(int n, String s) {
+        int valleys = 0;
+        int altitude = 0;
+        boolean valley = false;
+        int counter = 0;
+
+        String[] steps = s.split("");
+
+
+        for (int i = 1; i < steps.length; i++) {
+            if(steps[i].equals("D"))    {
+                altitude--;
+            }   else {
+                altitude++;
+            }
+            if (altitude >= 0) {
+                valley = false;
+                counter = 0;
+            }
+            else{
+                valley = true;
+                counter++;
+                if (counter == 1) {
+                    valleys++;
+                }
+            }
+        }
+        return valleys;
+
+    }
+
 }
