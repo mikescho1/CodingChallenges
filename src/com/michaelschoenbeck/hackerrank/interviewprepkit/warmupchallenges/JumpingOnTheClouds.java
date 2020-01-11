af00ceed453f3f7819328a3733ea18709ad17ba9
mike <mike.schoenbeck@gmail.com>
2020-01-11 18:38:50 -0500

    completed jumping on clouds

--- src/com/michaelschoenbeck/hackerrank/interviewprepkit/warmupchallenges/JumpingOnTheClouds.java
+++ src/com/michaelschoenbeck/hackerrank/interviewprepkit/warmupchallenges/JumpingOnTheClouds.java
@@ -1,4 +1,26 @@
 package com.michaelschoenbeck.hackerrank.interviewprepkit.warmupchallenges;
 
 public class JumpingOnTheClouds {
+    public static int jumpingOnClouds(int[] c) {
+        int jumps = 0;
+        int i = 0;
+        while(i!=c.length-1) {
+            if(i+1 == c.length-1)    {
+                jumps++;
+                break;
+            }
+            if(c[i+2] == 0) {
+                jumps++;
+                i++;
+            }
+            else    {
+                jumps++;
+            }
+            i++;
+        }
+        return jumps;
+
+
+    }
+
 }
