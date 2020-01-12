d8d1e7d361257334a9168adc317b20141c5295d5
mike <mike.schoenbeck@gmail.com>
2020-01-11 21:21:30 -0500

    completed leftRotationArray

--- src/com/michaelschoenbeck/hackerrank/interviewprepkit/arrays/LeftRotationArray.java
+++ src/com/michaelschoenbeck/hackerrank/interviewprepkit/arrays/LeftRotationArray.java
@@ -1,4 +1,16 @@
 package com.michaelschoenbeck.hackerrank.interviewprepkit.arrays;
 
 public class LeftRotationArray {
+
+    public static int[] rotLeft(int[] a, int d) {
+        int[] result = new int[a.length];
+
+        for(int i = 0; i<a.length; i++) {
+            if(i < a.length - d)  {
+                result[i] = a[i+d];
+            } else  {
+                result[i] = a[i+d-a.length] ;
+            }
+        }   return result;
+    }
 }
