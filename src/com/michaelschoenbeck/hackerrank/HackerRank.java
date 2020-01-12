e0e6ebe7ede210b703dcfa705080a116983857bc
mike <mike.schoenbeck@gmail.com>
2020-01-11 20:56:19 -0500

    completed repeated strings

--- src/com/michaelschoenbeck/hackerrank/HackerRank.java
+++ src/com/michaelschoenbeck/hackerrank/HackerRank.java
@@ -1,14 +1,18 @@
 package com.michaelschoenbeck.hackerrank;
 
 import com.michaelschoenbeck.hackerrank.interviewprepkit.warmupchallenges.CountingValleys;
+import com.michaelschoenbeck.hackerrank.interviewprepkit.warmupchallenges.RepeatedString;
 
 import static com.michaelschoenbeck.hackerrank.interviewprepkit.warmupchallenges.CountingValleys.countingValleys;
+import static com.michaelschoenbeck.hackerrank.interviewprepkit.warmupchallenges.RepeatedString.repeatedString;
 
 public class HackerRank {
 
+
+
     public static void main(String[] args) {
 
-        System.out.println(countingValleys(12, "DDUUDDUDUUUD"));
+        System.out.println(repeatedString("aab", 882787));
 
     }
 }
--- src/com/michaelschoenbeck/hackerrank/interviewprepkit/warmupchallenges/RepeatedString.java
+++ src/com/michaelschoenbeck/hackerrank/interviewprepkit/warmupchallenges/RepeatedString.java
@@ -1,4 +1,32 @@
 package com.michaelschoenbeck.hackerrank.interviewprepkit.warmupchallenges;
 
 public class RepeatedString {
+
+
+    public static long repeatedString(String s, long n) {
+        long counter = 0;
+        int i = 0;
+
+        if (s.equals("a")) {
+            counter = n;
+        } else {
+            for (i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == 'a') {
+                    counter++;
+                }
+            }
+            long numOfSs = (n / s.length());
+            long remainingLetters = (n % s.length());
+            counter *= numOfSs;
+
+            i = 0;
+            while (i < remainingLetters) {
+                if (s.charAt(i) == 'a') {
+                    counter++;
+                }
+                i++;
+            }
+        }
+        return counter;
+    }
 }
